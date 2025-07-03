efa03dc9f5e39d7ab0c8e09fbe1fe62fd15836ed
michal-mm <michal@Magic.local>
2025-07-03 16:59:23 +0200

    fixing workspace

--- leetcode/src/strings/TestStringSwap.java
+++ leetcode/src/strings/TestStringSwap.java
@@ -6,4 +6,14 @@ public class TestStringSwap extends TestCase {
         StringSwap sw = new StringSwap();
         assertEquals(true, sw.areAlmostEqual("bank", "knab"));
     }
+
+    public void testSpec2 () {
+        StringSwap sw = new StringSwap();
+        assertEquals(false, sw.areAlmostEqual("attac", "defend"));
+    }
+
+    public void testSpec3 () {
+        StringSwap sw = new StringSwap();
+        assertEquals(true, sw.areAlmostEqual("kelb", "kelb"));
+    }
 }
