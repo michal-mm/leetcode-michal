9a4dad7a6ae121d2a1ed1128af3bc2361a192e1a
michal-mm <michal@Magic.local>
2025-09-01 13:44:14 +0200

    divide array into sets of k-length consequitive elems

---
+++ leetcode/src/arrays/DivideArrayInSets.java
@@ -0,0 +1,64 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DivideArrayInSets {
+
+	/**
+	 * Given an array of integers nums and a positive integer k, 
+	 * check whether it is possible to divide this array 
+	 * into sets of k consecutive numbers.
+	 * 
+	 * Return true if it is possible. Otherwise, return false.
+	 * 
+	 * Example 1:
+	 * Input: nums = [1,2,3,3,4,4,5,6], k = 4
+	 * Output: true
+	 * Explanation: Array can be divided into [1,2,3,4] and [3,4,5,6].
+	 * 
+	 * 
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+	public boolean isPossibleDivide(int[] nums, int k) {
+		
+		// edge case 
+		if (nums.length % k != 0) {
+			return false;
+		}
+
+		// count occurrences
+		Map<Integer, Integer> count = new HashMap<>();
+		for (int num: nums) {
+			count.put(num, count.getOrDefault(num, 0)+1);
+		}
+		
+		List<Integer> uniqueNums = new ArrayList<>(count.keySet());
+		Collections.sort(uniqueNums);
+		
+		// check each unique item and see if we can build k-consequtive sets
+		// we check them by groups of their frequencies
+		for (int start: uniqueNums) {
+			int freq = count.get(start);
+			if (freq > 0) {
+				for (int i=0; i<k; i++) {
+					int curr = start+i;
+					if (count.getOrDefault(curr, 0) < freq) {
+						return false;
+					}
+					// decrease thre freq for the current elem
+					count.put(curr, count.get(curr)-freq);
+				}
+			}
+		}
+		
+		return true;
+	}
+}
---
+++ leetcode/tst/arrays/DivideArrayInSetsTest.java
@@ -0,0 +1,63 @@
+package arrays;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DivideArrayInSetsTest {
+
+	@Test
+	public void testSpec1() {
+		int[] nums = {1,2,3,3,4,4,5,6};
+		int k = 4;
+		boolean expected = true;
+		
+		DivideArrayInSets dais = new DivideArrayInSets();
+		
+		assertEquals(expected, dais.isPossibleDivide(nums, k));
+	}
+	
+	@Test
+	public void testSpec2() {
+		int[] nums = {3,2,1,2,3,4,3,4,5,9,10,11};
+		int k = 3;
+		boolean expected = true;
+		
+		DivideArrayInSets dais = new DivideArrayInSets();
+		
+		assertEquals(expected, dais.isPossibleDivide(nums, k));
+	}
+	
+	@Test
+	public void testSpec3() {
+		int[] nums = {1,2,3,4};
+		int k = 3;
+		boolean expected = false;
+		
+		DivideArrayInSets dais = new DivideArrayInSets();
+		
+		assertEquals(expected, dais.isPossibleDivide(nums, k));
+	}
+	
+	@Test
+	public void testSpec4() {
+		int[] nums = {1,2,3,4,5,7};
+		int k = 3;
+		boolean expected = false;
+		
+		DivideArrayInSets dais = new DivideArrayInSets();
+		
+		assertEquals(expected, dais.isPossibleDivide(nums, k));
+	}
+	
+	@Test
+	public void testSpec5() {
+		int[] nums = {1,2,3,1,5,7};
+		int k = 3;
+		boolean expected = false;
+		
+		DivideArrayInSets dais = new DivideArrayInSets();
+		
+		assertEquals(expected, dais.isPossibleDivide(nums, k));
+	}
+}
