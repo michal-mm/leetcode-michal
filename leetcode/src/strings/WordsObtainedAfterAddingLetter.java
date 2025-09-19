5dace48fe82a9f9b116c83ccaf98be2b526467f5
michal-mm <michal@Mac.lan>
2025-09-19 12:52:45 +0200

    works, but in a little brute-force fashion and has potential to optimize (likely use bitmask rather than an array to store "bits"). Passes all but 2 tests on leetcode (the 2 get time limit exceed).

---
+++ leetcode/src/strings/WordsObtainedAfterAddingLetter.java
@@ -0,0 +1,63 @@
+package strings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordsObtainedAfterAddingLetter {
+
+    public int wordCount(String[] startWords, String[] targetWords) {
+        int result = 0;
+        Map<Integer, List<String>> numOfLetters2Strings = new HashMap<>();
+
+        for (String startW : startWords) {
+            numOfLetters2Strings
+                    .computeIfAbsent(startW.length(), _ -> new ArrayList<>())
+                    .add(startW);
+        }
+
+        for (String word: targetWords) {
+            List<String> prefixes = numOfLetters2Strings.getOrDefault(word.length()-1, new ArrayList<>());
+            for (String prefix: prefixes) {
+                // do a check for each word against the "word"
+                int[] wordOccurence = charOccurenceMap(word);
+                int[] prefixOccurence = charOccurenceMap(prefix);
+                if (onlyOnePositionIsDifferent(wordOccurence, prefixOccurence)) {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean onlyOnePositionIsDifferent(int[] target, int[] prefix) {
+        int numOfSameElems = 0;
+        int differentElem = -1;
+
+        for (int i=0; i<target.length; i++) {
+            if (target[i] == prefix[i]) {
+                numOfSameElems++;
+            } else {
+                differentElem = i;
+            }
+        }
+
+//        System.out.println("same elems: " + numOfSameElems + " delta elem: " + differentElem);
+
+        return numOfSameElems == target.length-1
+                && target[differentElem] == prefix[differentElem] + 1;
+    }
+
+    private int[] charOccurenceMap(String s) {
+        int[] result = new int[26];
+
+        for (char c: s.toCharArray()) {
+            result[c-'a']++;
+        }
+
+        return result;
+    }
+}
---
+++ leetcode/tst/strings/WordsObtainedAfterAddingLetterTest.java
@@ -0,0 +1,30 @@
+package strings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WordsObtainedAfterAddingLetterTest {
+
+    @Test
+    public void testSpec1() {
+        String[] startWords = {"ant","act","tack"};
+        String[] targetWords = {"tack","act","acti"};
+        int expected = 2;
+
+        WordsObtainedAfterAddingLetter woaal = new WordsObtainedAfterAddingLetter();
+
+        assertEquals(expected, woaal.wordCount(startWords, targetWords));
+    }
+
+    @Test
+    public void testSpec2() {
+        String[] startWords = {"ab","a"};
+        String[] targetWords = {"abc","abcd"};
+        int expected = 1;
+
+        WordsObtainedAfterAddingLetter woaal = new WordsObtainedAfterAddingLetter();
+
+        assertEquals(expected, woaal.wordCount(startWords, targetWords));
+    }
+}
