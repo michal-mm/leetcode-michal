82d561e2060749c59ad6b92c65b9965837886199
michal-mm <michal@Magic.local>
2025-10-10 15:44:42 +0200

    Days without meetings problem -  brute force solution (O(n^2), where n is number of days).

---
+++ leetcode/tst/arrays/DaysWithoutMeetingsTest.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DaysWithoutMeetingsTest {
+
+    @Test
+    public void testSpec1() {
+        int days = 10;
+        int[][] meetings= {{5,7},{1,3},{9,10}};
+        int expected = 2;
+
+        DaysWithoutMeetings dwm = new DaysWithoutMeetings();
+
+        assertEquals(expected, dwm.countDays(days, meetings));
+    }
+
+    @Test
+    public void testSpec2() {
+        int days = 5;
+        int[][] meetings= {{2,4},{1,3}};
+        int expected = 1;
+
+        DaysWithoutMeetings dwm = new DaysWithoutMeetings();
+
+        assertEquals(expected, dwm.countDays(days, meetings));
+    }
+}
