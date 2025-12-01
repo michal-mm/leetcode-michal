239327c185e9eade2821b4fbd164c85f3c7cfbc0
michal-mm <michal.miotk@gmail.com>
2025-12-01 18:52:30 +0100

    Number of special characters - faster implementation without streams

--- leetcode/src/strings/NumOfSpecialCharacters.java
+++ leetcode/src/strings/NumOfSpecialCharacters.java
@@ -2,6 +2,7 @@ package strings;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class NumOfSpecialCharacters {
@@ -25,20 +26,25 @@ public class NumOfSpecialCharacters {
     }
 
     public int numberOfSpecialCharsV2(String word) {
-        Set<Character> lowerCase = new HashSet<>();
-        Set<Character> upperCase = new HashSet<>();
+        boolean[] foundLower = new boolean[26];
+        boolean[] foundUpper = new boolean[26];
+        boolean[] results = new boolean[26];
 
-        IntStream.range(0, word.length())
-                .mapToObj(word::charAt)
-                .forEach(c -> {
-                    if (Character.isLowerCase(c))
-                        lowerCase.add(c);
-                    if (Character.isUpperCase(c))
-                        upperCase.add(Character.toLowerCase(c));
-                });
+        AtomicInteger result = new AtomicInteger(0);
 
-        lowerCase.retainAll(upperCase);
+        int bound = word.length();
+        for (int i = 0; i < bound; i++) {
+            char c = word.charAt(i);
+            if (Character.isLowerCase(c)) foundLower[c - 'a'] = true;
+            if (Character.isUpperCase(c)) foundUpper[c - 'A'] = true;
+            if (foundLower[Character.toLowerCase(c) - 'a'] &&
+                    foundUpper[Character.toUpperCase(c) - 'A'] &&
+                    !results[Character.toLowerCase(c) - 'a']) {
+                results[Character.toLowerCase(c) - 'a'] = true;
+                result.incrementAndGet();
+            }
+        }
 
-        return lowerCase.size();
+        return result.get();
     }
 }
